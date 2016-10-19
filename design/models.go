cdeb1e740742e5b926e0356eb499cfe2a5aeaa88
Illya Busigin <illya.busigin@ingaged.co>
2016-10-19 12:15:07 -0500

    Adding gorma dependency

---
+++ design/models.go
@@ -0,0 +1,38 @@
+package design
+
+import (
+	cellar "github.com/goadesign/goa-cellar/design"
+	"github.com/goadesign/gorma"
+	. "github.com/goadesign/gorma/dsl"
+)
+
+var _ = StorageGroup("Cellar", func() {
+	Description("This is the global storage group")
+	Store("postgres", gorma.Postgres, func() {
+		Description("This is the Postgres relational store")
+		Model("Account", func() {
+			RendersTo(cellar.Account)
+			Description("Cellar Account")
+			Field("id", gorma.Integer, func() {
+				PrimaryKey()
+			})
+			Field("name", gorma.String)
+			HasMany("Bottles", "Bottle")
+		})
+
+		Model("Bottle", func() {
+			BuildsFrom(func() {
+				Payload("bottle", "create")
+				Payload("bottle", "update")
+			})
+			RendersTo(cellar.Bottle)
+			Field("id", gorma.Integer, func() {
+				PrimaryKey()
+			})
+			Field("rating", gorma.Integer)
+			Description("Bottle Model")
+			BelongsTo("Account")
+		})
+
+	})
+})
